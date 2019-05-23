d245eec0327d4c91ea8578c0403711ada3800472
nixlovemi <nixlovemi@gmail.com>
2019-05-23 11:27:00 -0300

    adicao dos headers Access-Control-Allow-Origin

--- application/controllers/Rest.php
+++ application/controllers/Rest.php
@@ -4,6 +4,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Rest extends CI_Controller {
   public function __construct(){
     CI_Controller::__construct();
+
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    $method = $_SERVER['REQUEST_METHOD'];
+    if($method == "OPTIONS") {
+      die();
+    }
   }
 
   // tb_base_despesa
